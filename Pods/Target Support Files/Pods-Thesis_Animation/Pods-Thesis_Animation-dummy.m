4b4e61f58cae21eb0ec6c8ca9b1fc1215fa4a45b
Emre Dogan <emredogan@outlook.com>
2022-04-28 12:55:41 +0200

    Completed the first animation page

---
+++ Pods/Target Support Files/Pods-Thesis_Animation/Pods-Thesis_Animation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Thesis_Animation : NSObject
+@end
+@implementation PodsDummy_Pods_Thesis_Animation
+@end
--- Thesis_Animation/ViewController.swift
+++ Thesis_Animation/ViewController.swift
@@ -6,13 +6,72 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var animationView: AnimationView!
+    @IBOutlet var mainBackground: UIView!
+    @IBOutlet weak var titleLabel: UIView!
+    @IBOutlet weak var getStartedViewBox: UIView!
+    @IBOutlet weak var getStartedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.stackView.alpha = 0.0
+        mainBackground.backgroundColor = UIColor(red: 145/255, green: 203/255, blue: 250/255, alpha: 1.0)
+        
+        animationView.backgroundColor = UIColor(red: 145/255, green: 203/255, blue: 250/255, alpha: 1.0)
+        
+        titleLabel.backgroundColor = UIColor(red: 145/255, green: 203/255, blue: 250/255, alpha: 1.0)
+        
+    
+        let animation = Animation.named("movie")
+        animationView.animation = animation
+        animationView.loopMode = .loop
+        animationView.play()
+        
+        addRoundShadow(view: getStartedViewBox)
+        addRoundShadow(view: getStartedButton)
+        
+        getStartedButton.addTarget(self, action: #selector(getStartedClicked), for: .touchUpInside)
+
+
+
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0) {
+                self.stackView.alpha = 1.0
+            }
+    }
+    
+    @objc func getStartedClicked(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.animationView.isHidden = true
+            
+            while let subview = self.getStartedViewBox.subviews.last {
+                subview.removeFromSuperview()
+            }
+            self.getStartedViewBox.center = self.mainBackground.center
+            self.getStartedViewBox.transform = CGAffineTransform(scaleX: 1, y: 1)
+            
+            
+        })
+    }
+    
+    func addRoundShadow(view: UIView) {
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 10
+        view.layer.cornerRadius = 10
+    }
+    
+    
+    
 
 
 }
